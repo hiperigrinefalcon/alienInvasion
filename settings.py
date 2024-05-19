b17429c23b3f6930bf283dd28d78e6f6bcfa98e9
hiperegrinefalcon <chung.manking@gmail.com>
2024-05-19 14:28:37 -0700

    bullet

--- settings.py
+++ settings.py
@@ -14,6 +14,6 @@ class Settings:
         # Bullet settings
         self.bullet_speed = 6.0
         self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_height = 150
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 30
